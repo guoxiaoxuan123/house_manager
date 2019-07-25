991966f6be325e1193c931fa872a888b7a93a0c4
guoxiaoxuan123 <3116792636@qq.com>
2019-07-25 20:44:16 +0800

    新加了DeptController

---
+++ src/main/java/com/aaa/house/controller/DeptController.java
@@ -0,0 +1,11 @@
+package com.aaa.house.controller;
+
+/**
+ * fileName:DeptController
+ * description:
+ * author:guoxiaoxuan
+ * createTime:2019/7/25 20:41
+ * versoin:1.0.0
+ */
+public class DeptController {
+}
